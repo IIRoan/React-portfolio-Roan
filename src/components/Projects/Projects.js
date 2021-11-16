af1152254ac7a283ef97e99a44652d7bf80f609b
IIRoan <contact@iroan.net>
2021-11-16 19:59:35 +0100

    Fixed links

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -8,8 +8,8 @@ export default function Projects() {
   return(
                 <div className={stylesprojects.projects}>
                 <a href="https://mineglr.nl"><img className={stylesprojects.zoom} src={mcmdb} alt="MCMDB"/></a>
-                <a href="https://react.iroan.tech"><img className={stylesprojects.zoom} src={react} alt="React"/></a>
-                <a href="https://iroan.tech"><img className={stylesprojects.zoom} src={mainweb} alt="Mainweb"/></a>
+                <a href="iroan.net"><img className={stylesprojects.zoom} src={react} alt="React"/></a>
+                <a href="https://iroan.net"><img className={stylesprojects.zoom} src={mainweb} alt="Mainweb"/></a>
                 </div>
   );
 }
